24108fc27a97d1050de730684971a0fe8cccf681
Sumairahmedzaheer <sumairahmed33@gmail.com>
2021-07-01 07:18:33 -0700

    portfolio

--- public/script.js
+++ public/script.js
@@ -4,7 +4,34 @@ $(document).ready(function(){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
+
         }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    //slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+
+    });
+
+    //typing animation script
+    var typed = new Typed(".typing",{
+        strings: ["Freelancer" , "Developer",  "Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2",{
+        strings: ["Freelancer" , "Developer", "Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // toggle menu/navbar script
@@ -13,4 +40,29 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
         
     });
+
+    //owl carousel script
+    $('.carousel').owlCarousel({
+        
+        margin: 20,
+        loop: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav:false
+            },
+            600:{
+                items: 2,
+                nav:false
+            },
+            1000:{
+                items: 3,
+                nav:false
+            }
+        }
+
+    });
+
 });
